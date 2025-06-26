92fceece5da8d77de31bcc10a1d8d6e6ed835191
satwiksahoo <satwiksahoojob@gmail.com>
2025-06-26 20:04:10 +0530

    unit test cases updated

---
+++ tests/test_operations.py
@@ -0,0 +1,11 @@
+from src.math_operations import add , sub
+
+
+def test_ad():
+    assert add(2,3) == 5
+    assert add(-1 ,1) == 0
+
+
+def test_sub():
+    assert sub(5,3) == 2
+    assert sub(6,5) == 1
